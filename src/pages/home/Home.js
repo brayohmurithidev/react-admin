544fbdf9d0c9fce36a0fc4ef9e67964a9e95cd5b
brayohmurithidev <murithibrianm@gmail.com>
2022-05-14 20:09:24 +0300

    Chart component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import { data } from "../../dummyData";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart title="Sales Analytics" data={data} dataKey="name" grid />
     </div>
   );
 };
