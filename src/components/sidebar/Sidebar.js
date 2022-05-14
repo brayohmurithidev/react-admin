2c04d85c056c64ac7a497473fb7329c48e37619a
brayohmurithidev <murithibrianm@gmail.com>
2022-05-14 12:24:15 +0300

    created sidebar menu

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,99 @@
+import React from "react";
+import "./sidebar.css";
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  Leaderboard,
+  Paid,
+  Group,
+  Inventory,
+  MailOutline,
+  ChatBubbleOutline,
+  RateReview,
+  ReportGmailerrorred,
+  ManageAccounts,
+} from "@mui/icons-material/";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyle className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" />
+              Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <Group className="sidebarIcon" />
+              Users
+            </li>
+            <li className="sidebarListItem">
+              <Inventory className="sidebarIcon" />
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <Paid className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <Leaderboard className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <MailOutline className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <RateReview className="sidebarIcon" />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutline className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <ManageAccounts className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <ReportGmailerrorred className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
